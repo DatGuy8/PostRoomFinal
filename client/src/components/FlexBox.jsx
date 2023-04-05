cdb7ac4dace8ae183892dd66a2686634cc263958
DatGuy8 <111431427+DatGuy8@users.noreply.github.com>
2023-04-05 02:11:05 -0700

    started on client side

---
+++ client/src/components/FlexBox.jsx
@@ -0,0 +1,10 @@
+import {Box, styled} from '@mui/system';
+
+
+const FlexBox= styled(Box)({
+  display:'flex',
+  justifyContent:"space-between",
+  alignItems:"center",
+})
+
+export default FlexBox;
