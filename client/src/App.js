fda7c3d3e92e3916ec24500bb19ce42797e2c1f1
DatGuy8 <111431427+DatGuy8@users.noreply.github.com>
2023-04-05 14:53:28 -0700

    add some redux stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import FlexBox from "./components/FlexBox";
+import FlexBox from 'components/FlexBox';
 import {Typography} from '@mui/material';
 
 
 function App() {
   return (
-    <FlexBox>
-      <Typography>test</Typography>
-      <Typography>test</Typography>
+    <FlexBox p='1rem 6%'>
+      <Typography>PostRoom</Typography>
+      <Typography>Login</Typography>
     </FlexBox>
   );
 }
